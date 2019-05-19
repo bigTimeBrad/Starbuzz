ed5104e0eb397b8a702d15085b8ebeda2ee432b0
Brad Nissen <bradanissen@gmail.com>
2019-05-19 11:39:49 -0700

    display Drink.drink[itemSelected] when the user selects a drink

--- app/src/main/java/com/bradnissen/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/bradnissen/starbuzz/DrinkActivity.java
@@ -2,12 +2,37 @@ package com.bradnissen.starbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DRINKNO = "drinkNo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        //get the extra information from the intent
+        int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
+
+        //This gets the object from the Drink array passing the the Drink No. from the intent
+        Drink drink = Drink.drinks[drinkNo];
+
+        //update the views with the data
+        updateViews(drink);
+    }
+
+    private void updateViews(Drink drink){
+        //get the views
+        ImageView img = (ImageView) findViewById(R.id.photo);
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView desc = findViewById(R.id.description);
+
+        //set the views
+        img.setImageResource(drink.getImageResourceId());
+        name.setText(drink.getName());
+        desc.setText(drink.getDescription());
     }
 }
--- app/src/main/java/com/bradnissen/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/bradnissen/starbuzz/DrinkCategoryActivity.java
@@ -3,6 +3,7 @@ package com.bradnissen.starbuzz;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -30,10 +31,12 @@ public class DrinkCategoryActivity extends ListActivity {
     /**
      * Use the onItemClick built-in method instead of creating our own.
      */
-    public void onItemClick(ListView listView, View itemView, int postition, long id)
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int postition, long id)
     {
+        Log.v("TAG", "User clicked "+ id);
         Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
-        intent.putExtra(DrinkActivty.EXTRA_DRINKNO, (int) id);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
         startActivity(intent);
     }
 
